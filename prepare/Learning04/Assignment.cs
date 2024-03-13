56d91671a0cde5b3f82ac6d1253fae2080dcbb5d
“moultoncarter” <mou21018@byui.edu>
2024-03-13 03:06:22 -0600

    learning activity 4 done

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _name;
+    private string _topic;
+
+    public Assignment(string name, string topic)
+    {
+        _name = name;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return ($"{_name} - {_topic}");
+
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment testAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment testMath = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssignment testWriting = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(testAssignment.GetSummary());
+        Console.WriteLine(testMath.GetSummary());
+        Console.WriteLine(testMath.GetHomeworkList());
+        Console.WriteLine(testWriting.GetSummary());
+        Console.WriteLine(testWriting.GetWritingInformation());
     }
 }
